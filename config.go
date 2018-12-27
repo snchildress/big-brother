64fbcde1480939506609802d1dbfc7b758807cf4
snchildress <steven.n.childress@gmail.com>
2018-12-26 18:45:04 -0600

    Add the Uber price estimate endpoint as a config

--- config.go
+++ config.go
@@ -7,6 +7,7 @@ var uberAPIKey = os.Getenv("UBER_API_KEY")
 var lyftAPIKey = os.Getenv("LYFT_API_KEY")
 
 // API Endpoint
+var uberEndpoint = "https://api.uber.com/v1.2/estimates/price"
 var lyftEndpoint = "https://api.lyft.com/v1/cost"
 
 // Trip origin and destination coordinates
