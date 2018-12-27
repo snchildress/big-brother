e6fa088b0b6795be55a1011bf790d620772dc7b9
snchildress <steven.n.childress@gmail.com>
2018-12-26 18:43:58 -0600

    Make the Lyft API request to get estimated cost

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -12,6 +16,21 @@ type Response struct {
 
 // Handler is the main AWS Lambda handler func
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// GET the Lyft estimated cost
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", lyftEndpoint, nil)
+	queryString := req.URL.Query()
+	queryString.Add("start_lat", startLat)
+	queryString.Add("start_lng", startLong)
+	queryString.Add("end_lat", endLat)
+	queryString.Add("end_lng", endLong)
+	req.URL.RawQuery = queryString.Encode()
+	req.Header.Set("Authorization", "bearer "+lyftAPIKey)
+	res, _ := client.Do(req)
+	resBodyBytes, _ := ioutil.ReadAll(res.Body)
+	resBody := string(resBodyBytes)
+	fmt.Println(resBody)
+
 	return events.APIGatewayProxyResponse{
 		Body:       "Hello, World!",
 		StatusCode: 200,
