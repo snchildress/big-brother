de63f98158b866f8113d6aeec8c3f4ed60e0f311
snchildress <steven.n.childress@gmail.com>
2018-12-27 11:31:05 -0600

    Refactor to handle estimations as private function

--- main.go
+++ main.go
@@ -14,39 +14,75 @@ import (
 
 // Handler is the main AWS Lambda handler func
 func Handler() (response events.APIGatewayProxyResponse, err error) {
-	// GET the Lyft estimated cost
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", lyftEndpoint, nil)
+	// Get the Lyft estimate
+	estimate, err := getEstimate(true)
 	if err != nil {
 		return
 	}
+
+	// Return the Lyft estimate
+	response = events.APIGatewayProxyResponse{
+		Body:       estimate,
+		StatusCode: 200,
+	}
+	return
+}
+
+// getEstimate requests an estimated price for the configured
+// coordinates for the given service, Lyft or Uber
+func getEstimate(lyft bool) (estimate string, err error) {
+	// Create an HTTP client for the given service's endpoint and key
+	client := &http.Client{}
+	endpoint := uberEndpoint
+	if lyft {
+		endpoint = lyftEndpoint
+	}
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return
+	}
+
+	// Set the headers and query string accordingly
 	queryString := req.URL.Query()
-	queryString.Add("start_lat", startLat)
-	queryString.Add("start_lng", startLong)
-	queryString.Add("end_lat", endLat)
-	queryString.Add("end_lng", endLong)
+	if lyft {
+		req.Header.Set("Authorization", "bearer "+lyftAPIKey)
+		queryString.Add("start_lat", startLat)
+		queryString.Add("start_lng", startLong)
+		queryString.Add("end_lat", endLat)
+		queryString.Add("end_lng", endLong)
+	} else {
+		req.Header.Set("Authorization", "Token "+uberAPIKey)
+		queryString.Add("start_latititude", startLat)
+		queryString.Add("start_longitude", startLong)
+		queryString.Add("end_latitude", endLat)
+		queryString.Add("end_longitude", endLong)
+	}
 	req.URL.RawQuery = queryString.Encode()
-	req.Header.Set("Authorization", "bearer "+lyftAPIKey)
+
+	// Make the API request
 	res, err := client.Do(req)
 	if err != nil {
 		return
 	}
-	resBodyBytes, _ := ioutil.ReadAll(res.Body)
-	resBody := string(resBodyBytes)
-	data := map[string]interface{}{}
-	dec := json.NewDecoder(strings.NewReader(resBody))
-	dec.Decode(&data)
-	jq := jsonq.NewQuery(data)
-	lyftEstimate, _ := jq.Object("cost_estimates", "0")
-	lyftMinPriceFloat := lyftEstimate["estimated_cost_cents_min"].(float64) / 100
-	lyftMaxPriceFloat := lyftEstimate["estimated_cost_cents_max"].(float64) / 100
-	lyftMinPrice := fmt.Sprintf("%.0f", lyftMinPriceFloat)
-	lyftMaxPrice := fmt.Sprintf("%.0f", lyftMaxPriceFloat)
-	lyftEstimateMessage := "Lyft Estimate: $" + lyftMinPrice + "-" + lyftMaxPrice
 
-	response = events.APIGatewayProxyResponse{
-		Body:       lyftEstimateMessage,
-		StatusCode: 200,
+	// Get the response body
+	resBodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	resBodyString := string(resBodyBytes)
+	resBodyMap := map[string]interface{}{}
+	json.NewDecoder(strings.NewReader(resBodyString)).Decode(&resBodyMap)
+	resBody := jsonq.NewQuery(resBodyMap)
+
+	// Get the estimate from the response body
+	if lyft {
+		lyftEstimate, _ := resBody.Object("cost_estimates", "0")
+		lyftMinPriceFloat := lyftEstimate["estimated_cost_cents_min"].(float64) / 100
+		lyftMaxPriceFloat := lyftEstimate["estimated_cost_cents_max"].(float64) / 100
+		lyftMinPrice := fmt.Sprintf("%.0f", lyftMinPriceFloat)
+		lyftMaxPrice := fmt.Sprintf("%.0f", lyftMaxPriceFloat)
+		estimate = "Lyft Estimate: $" + lyftMinPrice + "-" + lyftMaxPrice
 	}
 	return
 }
